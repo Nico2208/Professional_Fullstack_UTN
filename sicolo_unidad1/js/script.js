4f18dbeac70b0829529454505e8c897101e9fed7
Nicolas Sicolo <sicolo2208@gmail.com>
2022-03-10 09:58:30 -0300

    nuevos files y modificaciones

--- sicolo_unidad1/js/script.js
+++ sicolo_unidad1/js/script.js
@@ -2,13 +2,13 @@ function mostrarValorSeguro () {
     var tipoSeguro = document.getElementById('tipoSeguro').value;
     switch(tipoSeguro){
         case "1":
-            document.getElementById("precioSeguro").innerHTML="$ 500"
+            document.getElementById("precioSeguro").innerHTML="El valor del seguro seleccionado es $ 500"
             break
         case "2":
-            document.getElementById("precioSeguro").innerHTML="$ 1000"
+            document.getElementById("precioSeguro").innerHTML="El valor del seguro seleccionado es $ 1000"
             break;
         case "3":
-            document.getElementById("precioSeguro").innerHTML="$ 1500"
+            document.getElementById("precioSeguro").innerHTML="El valor del seguro seleccionado es $ 1500"
             break;
     }
 }

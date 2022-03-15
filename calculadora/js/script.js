a956a66e8c450a97e1dd652d4ab3fa90071074ff
Nicolas Sicolo <sicolo2208@gmail.com>
2022-03-15 17:19:26 -0300

    modifs realizadas

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,5 +1,15 @@
-function mostrarNumero (numero) {
+const mostrarNumero = numero => {
     var num = document.getElementById(numero).innerHTML;
     console.log(num);
     document.getElementById('display').innerHTML += num;
+}
+
+const mostrarSimbolo = simbolo => {
+    var simb = document.getElementById(simbolo).innerHTML;
+    document.getElementById('display').innerHTML += simb;
+}
+
+const calcular = () => {
+    var display = parseInt(document.getElementById('display').innerHTML);
+    console.log(display);
 }

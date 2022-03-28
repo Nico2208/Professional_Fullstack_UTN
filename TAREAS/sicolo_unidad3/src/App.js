3fd8b1e8ecc320ee29f42e0e85ef2c685adbb344
Nicolas Sicolo <sicolo2208@gmail.com>
2022-03-28 15:41:51 -0300

    nuevas modifs

--- TAREAS/sicolo_unidad3/src/App.js
+++ TAREAS/sicolo_unidad3/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
+  // App es un superior de Home porque App inyecta a Home
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Home esta siendo inyectado por App */}
+      {/* Home es un inferior de App porque home esta siendo inyectado por app */}
+      <Home/>
     </div>
   );
 }
---
+++ TAREAS/sicolo_unidad3/src/Home.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+class Home extends Component{
+    render () {
+        return (
+            <div>
+                <h1>Home MyApp</h1> 
+                <div>
+                    <p>Nombre: Silla Maria</p>
+                    <p>Descripcion: Silla nordica con terminaciones no resueltas</p> 
+                    <p>Precio: $80.000</p> 
+                    <p>SKU: #F456</p>
+                    <p>Cantidad Disponible: 2</p>
+
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;

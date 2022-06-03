946329af543cea2b1ec96eb42406f39493639906
Nicolas Sicolo <sicolo2208@gmail.com>
2022-06-03 16:54:25 -0300

    cambios en productoService; coloco info sobre la api

--- TAREAS/MODULO 2/sicolo_unidad3/src/Services/ProductoService.js
+++ TAREAS/MODULO 2/sicolo_unidad3/src/Services/ProductoService.js
@@ -4,6 +4,9 @@ export async function getAllProductos(filtrar){
     return axios.get("/sites/MLA/search?q="+filtrar) 
 }
 export async function getByIdProductos(id){
-    return axios.get("/items/"+id) 
+    return axios.get("/items/"+id) //tengo que poner items porque asi esta determinado en la api de mercadolibre
+    // Segun lo indicado en la api de ML
+    // Retrieves the information of an item; to retrieve means recuperar
+    // /items/:id
 }
 

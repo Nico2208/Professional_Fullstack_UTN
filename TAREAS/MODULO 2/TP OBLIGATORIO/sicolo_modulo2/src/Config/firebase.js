e74c009f6447f414799d2d004451105be066e6d5
Nicolas Sicolo <sicolo2208@gmail.com>
2022-06-14 11:37:24 -0300

    nuevas modifs realizadas

---
+++ TAREAS/MODULO 2/TP OBLIGATORIO/sicolo_modulo2/src/Config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC0WK31wKSgQBsVXxS6CfC0Kq9hrkdBj0o",
+    authDomain: "chuchis2022-e301e.firebaseapp.com",
+    projectId: "chuchis2022-e301e",
+    storageBucket: "chuchis2022-e301e.appspot.com",
+    messagingSenderId: "71906047121",
+    appId: "1:71906047121:web:d665c6ed89adc629c47b30"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
--- TAREAS/MODULO 2/TP OBLIGATORIO/sicolo_modulo2/src/Pages/Home.js
+++ TAREAS/MODULO 2/TP OBLIGATORIO/sicolo_modulo2/src/Pages/Home.js
@@ -1,6 +1,8 @@
 import Productos from "../Components/Productos";
+import firebase from "firebase/compat";
 
 function Home () {
+    console.log("firebase: ", firebase)
     return (
         <div>
             <h1>Bienvenidos</h1>
